e813fd7d26da9de852fb0a65e48f761c9b5a744d
shan1129 <sns1113@protonmail.com>
2024-11-07 12:38:47 -0500

    Problem Number 577

---
+++ 2. Basic Joins/Employee Bonus.sql
@@ -0,0 +1,8 @@
+-- Problem Number 577
+
+SELECT E.name, B.bonus
+FROM Employee E LEFT JOIN Bonus B 
+    ON E.empId  = B.empId
+WHERE B.bonus < 1000 OR B.bonus IS NULL
+
+-- More challenging 1142, 612, 613
