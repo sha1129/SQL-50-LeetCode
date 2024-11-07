538ce76f85fb6848338f4221fe04cfb9e91e59ad
shan1129 <sns1113@protonmail.com>
2024-11-07 13:41:52 -0500

    -- Problem number 1280

---
+++ 2. Basic Joins/Students and Examinations.sql
@@ -0,0 +1,14 @@
+-- Problem number 1280
+
+SELECT S.student_id, S.student_name, Sb.subject_name, COUNT(Ex.student_id) AS attended_exams
+FROM Students S
+CROSS JOIN Subjects Sb 
+LEFT JOIN Examinations Ex
+    ON S.student_id = Ex.student_id AND Sb.subject_name = Ex.subject_name
+GROUP BY S.student_id, S.student_name, Sb.subject_name
+ORDER BY S.student_id, Sb.subject_name ASC
+
+-- CROSS JOIN with Subjects to show all the subjects 
+-- LEFT JOIN to show the nulls 0
+
+-- More challenging problem 3150, 512, 2010 
