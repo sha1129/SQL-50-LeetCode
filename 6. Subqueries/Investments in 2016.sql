ae36f481ae7e55c8b9d4be2a3f0f2cafd9785c64
shan1129 <sns1113@protonmail.com>
2024-12-02 09:22:41 -0500

    Problem number 585

---
+++ 6. Subqueries/Investments in 2016.sql
@@ -0,0 +1,20 @@
+-- Problem number 585
+
+SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM Insurance
+WHERE tiv_2015 IN (
+    SELECT tiv_2015
+    FROM Insurance
+    GROUP BY tiv_2015
+    HAVING COUNT(*) > 1
+)
+AND (lat, lon) IN (
+    SELECT lat, lon
+    FROM Insurance
+    GROUP BY lat, lon
+    HAVING COUNT(*) = 1
+);
+
+
+-- Not sure about this tho
+-- 1322, 603, 1164
